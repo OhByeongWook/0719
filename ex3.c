1dd89d063d96b20c9aec73413e0fc903d9eab9ac
오병욱 <obw@192.168.nate.com>
2022-07-19 21:38:31 +0900

    first commit

---
+++ ex3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    short num1=1;
+    int num2=1;
+    long num3=1;
+
+    float num4=1.0;
+    double num5=1.0;
+
+    char str1='c';
+
+    printf("%d\n",num1);
+    printf("%d\n",num2);
+    printf("%1d\n",num3);
+    
+    printf("%f\n",num4);
+    printf("%1f\n",num5);
+
+    printf("%c",str1);
+}
